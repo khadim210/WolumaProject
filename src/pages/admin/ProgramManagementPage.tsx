3bdf44d9fb361eadfa745b9ffd457a6d46e4d836
MBACKE <ahmathbamba.mbacke@gmail.com>
2025-09-17 11:31:20 +0000

    Fix Button import from named to default

--- src/pages/admin/ProgramManagementPage.tsx
+++ src/pages/admin/ProgramManagementPage.tsx
@@ -16,7 +16,7 @@ import {
   DollarSign,
   Target
 } from 'lucide-react';
-import { Button } from '../../components/ui/Button';
+import Button from '../../components/ui/Button';
 import { Card } from '../../components/ui/Card';
 import Badge from '../../components/ui/Badge';
 import { useProgramStore } from '../../stores/programStore';
