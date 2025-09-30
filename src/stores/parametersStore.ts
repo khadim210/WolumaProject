d93ec0f66b0c5359b424638fc7b2a8da967b893a
MBACKE <ahmathbamba.mbacke@gmail.com>
2025-09-30 21:01:54 +0000

    Fix DatabaseManager import in parametersStore

--- src/stores/parametersStore.ts
+++ src/stores/parametersStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import { DatabaseManager } from '../utils/database';
 
 export interface SystemParameters {
   // General
